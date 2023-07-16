9775e793f8b49e7cba73d9883e11b8d9fcd50faf
Erxathos <32100741+Erxathos@users.noreply.github.com>
2023-07-16 23:27:49 +0200

    improve api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -13,7 +13,7 @@ def test_classify_response_schema(client):
     result = client.get("/get_prediction/google.com")
     assert result.status_code == 200
 
-    # TODO: get data from schema, test the schema
+    # TODO: test schema data, not constants
     keys = result.json["google.com"].keys()
     for key in ["classification", "p_dga", "p_legit"]:
         assert key in keys
@@ -25,18 +25,23 @@ def test_classify_invalid_url(client):
 
 
 @pytest.mark.parametrize(
-    "url_string, expected_class",
+    "url_string, expected_class, url_count",
     [
-        ("google.com yahoo.com bing.com", "legit"),
-        ("sflglfgldm.com lskgjflkd.org sfgskmdkfls.co.uk", "dga"),
+        ("google.com yahoo.com bing.com", "legit", 3),
+        ("sflglfgldm.com lskgjflkd.org sfgskmdkfls.co.uk", "dga", 3),
     ],
 )
-def test_batch_classify_sanity_check(url_string, expected_class, client):
+def test_batch_classify_sanity_check(url_string, expected_class, url_count, client):
     data = {"url_string": url_string}
     result = client.post("/get_predictions", json=data)
     assert result.status_code == 200
-    assert all([result.json[key]["classification"] == expected_class for key in result.json.keys()])
-    assert len(result.json) == 3
+    assert all(
+        [
+            result.json[key]["classification"] == expected_class
+            for key in result.json.keys()
+        ]
+    )
+    assert len(result.json) == url_count
 
 
 def test_batch_classify_invalid_urls(client):
